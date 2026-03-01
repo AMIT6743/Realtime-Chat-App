3f6642e6da79e17f5c1139ac57fa0d80cd6937e0
amitmeena9876 <amitmeena96879687@gmail.com>
2026-03-01 11:38:59 +0530

    Fix payload too large error for image uploads

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5001;
 
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
